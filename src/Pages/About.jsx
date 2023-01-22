b02db004fbee25f743993afc04cfd3273a87ebc5
sairaj04 <sairaj04480@gmail.com>
2023-01-22 13:09:00 +0100

    about page queries

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -20,16 +20,16 @@ function About() {
           </h1>
         </div>
         <div className="container page-padding py-[5rem]">
-          <div className="grid grid-cols-[1fr_1fr] h-[60rem]">
+          <div className="grid grid-cols-[1fr_1fr] h-[60rem] md1000:grid-cols-1">
             {/* title div -- */}
-            <div className="flex flex-col !text-left relative items-right justify-center">
-              <p className="text-white relative z-10 text-[15px] uppercase font-bold mb-8 ml-20 ">
+            <div className="flex flex-col !text-left relative items-right justify-center md1000:items-center md1000:!text-center">
+              <p className="text-white relative z-10 text-[15px] uppercase font-bold mb-8 ml-20 md1000:ml-3 ">
                 who we are
               </p>
               <img
                 src={TitleRed}
                 alt="text_bg"
-                className="w-[21rem] absolute top-[120px]"
+                className="w-[21rem] absolute top-[120px] min450:top-[84px] min375:top-[68px]"
               />
 
               <h2 className="text-[3.6rem] max-w-[53rem] font-bold mb-4">
@@ -51,7 +51,7 @@ function About() {
               />
             </div>
             {/*  */}
-            <div className="relative w-[80%] -top-[90px] left-[40px]">
+            <div className="relative w-[80%] -top-[90px] left-[40px] md1000:hidden">
               <img
                 src={GirlRunning}
                 alt="girl_running"
@@ -64,8 +64,8 @@ function About() {
           </div>
           {/*  */}
           <div className="mt-[10rem]">
-            <div className="grid grid-cols-2 grid-rows-2 w-full">
-              <div className="w-full bg-white h-[43rem] flex flex-col justify-center items-center text-center px-[100px]">
+            <div className="grid grid-cols-2 grid-rows-2 w-full min620:grid-cols-1">
+              <div className="w-full bg-white h-[43rem] flex flex-col justify-center items-center text-center px-[100px] min800:px-[10px]">
                 <img src={Target} alt="target_img" className="w-[9rem]" />
                 <h2 className="text-[36px] font-bold mt-3">Our History</h2>
                 <p className="text-[16px] font-medium text-[#646464] mt-5">
@@ -91,7 +91,7 @@ function About() {
                 />
               </div>
               {/*  */}
-              <div className="w-full bg-white h-[43rem] flex flex-col justify-center items-center text-center px-[100px]">
+              <div className="w-full bg-white h-[43rem] flex flex-col justify-center items-center text-center px-[100px] min800:px-[10px]">
                 <img src={Mountain} alt="target_img" className="w-[9rem]" />
                 <h2 className="text-[36px] font-bold mt-3">Our History</h2>
                 <p className="text-[16px] font-medium text-[#646464] mt-5">
